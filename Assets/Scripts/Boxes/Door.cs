31c9c5a249f8c31ebefc6953a6cf3af32ff82d04
Aaditya <aadisingh3217@gmail.com>
2026-01-25 07:02:26 +0530

    extra

--- Assets/Scripts/Boxes/Door.cs
+++ Assets/Scripts/Boxes/Door.cs
@@ -1,32 +1,59 @@
 using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
     private Collider2D col;
     private Animator anim;
     public bool isOpen = false;
-    [SerializeField] private GameObject WinningScene;
+    [SerializeField] private GameObject winningSceneCanvas;
 
     void Start()
     {
         col = GetComponent<Collider2D>();
         anim = GetComponent<Animator>();
+
+        if (col != null && !col.isTrigger)
+        {
+            Debug.LogWarning("Door collider is not set as trigger! Setting it now.");
+            col.isTrigger = true;
+        }
+
+        if (winningSceneCanvas != null)
+        {
+            winningSceneCanvas.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && isOpen)
         {
-            if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 1)
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            if (currentSceneIndex == 1)
             {
-                UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+                Debug.Log("Player entered door in scene 1. Loading scene 2.");
+                SceneManager.LoadScene(2);
             }
-            else if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 2)
+            else if (currentSceneIndex == 2)
             {
-                WinningScene.SetActive(true);
+                if (winningSceneCanvas != null)
+                {
+                    Debug.Log("Player entered door in scene 2. Enabling winning scene canvas.");
+                    winningSceneCanvas.SetActive(true);
+                }
+                else
+                {
+                    Debug.LogWarning("Winning scene canvas is not assigned!");
+                }
             }
+        }
+        else if (collision.CompareTag("Player") && !isOpen)
+        {
+            Debug.Log("Player tried to enter door but it's not open yet.");
         }
     }
 }
